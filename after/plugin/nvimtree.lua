7dc52867657c9d24863ec0007d71e40b2f248097
Sock, the dev <sockthedev@ctrlplusb.com>
2022-12-27 00:05:08 +0000

    improvements to nvim-tree

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -4,7 +4,15 @@
 
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
-  open_on_setup = true,
+  open_on_setup = false,
+  open_on_setup_file = false,
+  hijack_directories = {
+    enable = false,
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
   view = {
     adaptive_size = true,
     hide_root_folder = true,
@@ -20,6 +28,16 @@ require("nvim-tree").setup({
   filters = {
     dotfiles = false,
     custom = { '^.git$' },
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
   },
 })
 
