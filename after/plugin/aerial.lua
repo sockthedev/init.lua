4c54593725e30c76a4c8ed72622dd0a7f07fb49a
Sock, the dev <sockthedev@ctrlplusb.com>
2022-12-31 00:11:23 +0000

    fixes aerial toggling

--- after/plugin/aerial.lua
+++ after/plugin/aerial.lua
@@ -6,6 +6,7 @@ end
 
 -- configure aerial
 aerial.setup({
+  filter_kind = false,
   on_attach = function(bufnr)
     -- Jump forwards/backwards with '{' and '}'
     vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
