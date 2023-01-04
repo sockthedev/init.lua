af96aff5fb988b0fdb43b6c7eae83c5e74b030eb
Sock, the dev <sockthedev@ctrlplusb.com>
2023-01-04 01:37:52 +0000

    adds autopairs

--- lua/plugins/lsp/lightbulb.lua
+++ lua/plugins/lsp/lightbulb.lua
@@ -12,20 +12,11 @@ lightbulb.setup({
   },
 })
 
--- disabled because of unusual offset_encoding of `clangd` server causing warning in:
--- https://github.com/neovim/neovim/blob/88c32b5/runtime/lua/vim/lsp/util.lua#L1882-L1886
-local is_disabled_filetype = {
-  c = true,
-  cpp = true,
-}
-
 vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
   pattern = { "*" },
   group = vim.api.nvim_create_augroup("LightBulb", { clear = true }),
   desc = "lua require('nvim-lightbulb').update_lightbulb()",
   callback = function(params)
-    if not is_disabled_filetype[vim.bo[params.buf].filetype] then
-      lightbulb.update_lightbulb()
-    end
+    lightbulb.update_lightbulb()
   end,
 })
